09be61d5d0564290588dda352c6840ca679f24df
5hyun <qhslsl@gmail.com>
2022-12-26 23:04:13 +0900

    webpack 튜토리얼

---
+++ getting-started/webpack.config.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = {
+  mode: "none",
+  entry: "./src/index.js",
+  // webpack을 entry 있는 파일을 대상으 돌려서 output(결과물)을 내는 것
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
