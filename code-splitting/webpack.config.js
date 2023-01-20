bd5946ad3e881d14db2c0e5012f20493f2bdb894
5hyun <qhslsl@gmail.com>
2023-01-21 00:36:36 +0900

    Feat: 튜토리얼 완성

--- code-splitting/webpack.config.js
+++ code-splitting/webpack.config.js
@@ -1,18 +1,29 @@
 var path = require("path");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  // mode에는 production, development, none으로 되어있다.
+  // 배포할때는 항상 production을 써줘야한다.
   mode: "none",
   entry: "./index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  // 이것은 loader
   module: {
     rules: [
       {
+        // 모든 css 확장자를 가진 파일에 use에 있는 loader를 적용하겠다라는 의미
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
       },
+      //  모든 js 파일에 대해 babel을 돌리겠다는 의미
+      // {
+      //   test: /\.js$/,
+      //   use: ["babel-loader"],
+      // },
     ],
   },
+  plugins: [new MiniCssExtractPlugin()],
 };
